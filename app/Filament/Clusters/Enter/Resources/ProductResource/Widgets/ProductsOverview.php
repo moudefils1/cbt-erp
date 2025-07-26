e14826495cac4d35ef5505d0120f6284d3a48d31
Mahamoud BRAHIM ADOUM <moudefils@gmail.com>
2025-07-26 22:45:32 +0300

    Refactor ProductsOverview widget to enhance product statistics display and improve visual representation

--- app/Filament/Clusters/Enter/Resources/ProductResource/Widgets/ProductsOverview.php
+++ app/Filament/Clusters/Enter/Resources/ProductResource/Widgets/ProductsOverview.php
@@ -1,34 +1,79 @@
 <?php
 
-namespace App\Filament\Clusters\Enter\Resources\ProductResource\Widgets;
+    namespace App\Filament\Clusters\Enter\Resources\ProductResource\Widgets;
 
-use App\Models\Product;
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
-use Filament\Widgets\StatsOverviewWidget\Stat;
+    use App\Models\Product;
+    use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+    use Filament\Widgets\StatsOverviewWidget\Stat;
+    use Illuminate\Support\HtmlString;
 
-class ProductsOverview extends BaseWidget
-{
-    protected function getStats(): array
+    class ProductsOverview extends BaseWidget
     {
-        return [
-            Stat::make('En Stock', Product::where('product_type_id', 1)->where('is_available', 1)->count())
-                ->icon('heroicon-o-computer-desktop')
-                ->description('Matériels Électroniques')
-                ->descriptionColor('success'),
-            Stat::make('En Stock', Product::where('product_type_id', 2)->where('is_available', 1)->count())
-                ->icon('heroicon-o-truck')
-                ->description('Véhicules')
-                ->descriptionColor('success'),
-            Stat::make('En Stock', Product::where('product_type_id', 3)->where('is_available', 1)->sum('quantity'))
-                ->icon('heroicon-o-shopping-cart')
-                ->description('Vivres et Autres')
-                ->descriptionColor('success'),
-        ];
-    }
+        protected int | string | array $columnSpan = 'full';
 
-    // canView()
-    public static function canView(): bool
-    {
-        return auth()->user()->hasRole("super_admin") || auth()->user()->can('widget_ProductsOverview');
+        protected function getStats(): array
+            {
+                return [
+                    // Matériels Électroniques
+                    Stat::make('Matériels Électroniques', null)
+                        ->icon('heroicon-o-computer-desktop')
+                        ->color('primary')
+                        ->chart([
+                            Product::where('product_type_id', 1)->where('is_available', 1)->count(),
+                            Product::where('product_type_id', 1)->where('is_available', 0)->count(),
+                        ])
+                        ->extraAttributes([
+                            'class' => 'ring-2 ring-primary-50 shadow-md',
+                        ])
+                        ->description(new HtmlString("
+                            <span style='color: green;'>Disponible(s): " . Product::where('product_type_id', 1)->where('is_available', 1)->count() . "</span><br>
+                            <span style='color: red;'>Attribué(s): " . Product::where('product_type_id', 1)->where('is_available', 0)->count() . "</span>")
+                            ),
+
+                    // Véhicules
+                    Stat::make('Véhicules', null)
+                        ->icon('heroicon-o-truck')
+                        ->color('warning')
+                        ->chart([
+                            Product::where('product_type_id', 2)->where('is_available', 1)->count(),
+                            Product::where('product_type_id', 2)->where('is_available', 0)->count(),
+                        ])
+                        ->extraAttributes([
+                            'class' => 'ring-2 ring-warning-50 shadow-md',
+                        ])
+//                        ->description(
+//                            'En Stock: ' . Product::where('product_type_id', 2)->where('is_available', 1)->count() .
+//                            "\nAttribué(s): " . Product::where('product_type_id', 2)->where('is_available', 0)->count()
+//                        ),
+                        ->description(new HtmlString("
+                            <span style='color: green;'>Disponible(s): " . Product::where('product_type_id', 2)->where('is_available', 1)->count() . "</span><br>
+                            <span style='color: red;'>Attribué(s): " . Product::where('product_type_id', 2)->where('is_available', 0)->count() . "</span>")
+                        ),
+
+                    // Vivres et Autres
+                    Stat::make('Vivres et Autres', null)
+                        ->icon('heroicon-o-shopping-cart')
+                        ->color('success')
+                        ->chart([
+                            Product::where('product_type_id', 3)->where('is_available', 1)->sum('quantity'),
+                            Product::where('product_type_id', 3)->where('is_available', 0)->sum('quantity'),
+                        ])
+                        ->extraAttributes([
+                            'class' => 'ring-2 ring-success-50 shadow-md',
+                        ])
+//                        ->description(
+//                            'En Stock: ' . Product::where('product_type_id', 3)->where('is_available', 1)->sum('quantity') .
+//                            "\nAttribué(s): " . Product::where('product_type_id', 3)->where('is_available', 0)->sum('quantity')
+//                        ),
+                        ->description(new HtmlString("
+                            <span style='color: green;'>Disponible(s): " . Product::where('product_type_id', 3)->where('is_available', 1)->sum('quantity') . "</span><br>
+                            <span style='color: red;'>Attribué(s): " . Product::where('product_type_id', 3)->where('is_available', 0)->sum('quantity') . "</span>")
+                        ),
+                ];
+            }
+
+        public static function canView(): bool
+        {
+            return auth()->user()->hasRole("super_admin") || auth()->user()->can('widget_ProductsOverview');
+        }
     }
-}
